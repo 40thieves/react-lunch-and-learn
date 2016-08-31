1402962c69db316240cf195fcb929801f6748bcb
Alasdair Smith <ali@alasdairsmith.co.uk>
2016-08-31 11:31:42 +0100

    Initial commit with spectacle boilerplate

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+
+import LunchAndLearn from './presentation/lunch-and-learn'
+
+render(<LunchAndLearn />, document.getElementById("root"));
